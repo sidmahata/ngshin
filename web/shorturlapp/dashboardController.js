5895aaf64376d1f7a2067d764b13391cc04d63de
sidmahata <siddhartha.mahata@gmail.com>
2015-11-25 19:08:43 +0530

    shorten optimized but not working, case sensitive routing

--- web/shorturlapp/dashboardController.js
+++ web/shorturlapp/dashboardController.js
@@ -4,16 +4,16 @@
 
     var homeController = function($scope, $http){
 
-        var ongeturlComplete = function(response){
-            $scope.shorturl = response.data;
-        };
-
-        var onError = function(response){
-            $scope.error = "Could not fetch data";
-        };
-
-        $http.get("http://localhost/ngshin/web/app_dev.php/api/urls/1")
-            .then(ongeturlComplete, onError);
+//        var ongeturlComplete = function(response){
+//            $scope.shorturl = response.data;
+//        };
+//
+//        var onError = function(response){
+//            $scope.error = "Could not fetch data";
+//        };
+//
+//        $http.get("http://localhost/ngshin/web/app_dev.php/api/urls/1")
+//            .then(ongeturlComplete, onError);
 
         $scope.homemessage = "hello homepage , Its working";
     };
--- web/shorturlapp/shortenController.js
+++ web/shorturlapp/shortenController.js
@@ -6,18 +6,28 @@
 
         var onUserLoadComplete = function(response){
             $scope.shorturl = response.data;
+            $scope.error = null;
         };
 
         var onError = function(response){
             $scope.error = "Could not fetch data";
         };
 
-        $scope.shorten = function(){
+        $scope.shorten1 = function(){
             $http.post("http://localhost/shin/web/app_dev.php/api/urls", $scope.urlentry)
                 .then(onUserLoadComplete, onError);
         };
 
+        $scope.shorten = function(){
+            $http.get("https://api.github.com/users/sidmahata")
+                .then(onUserLoadComplete, onError);
+        };
+
+
+
         $scope.message = "hello world , Its working";
+        $scope.isAdvancedFormOpen = false;
+
     };
 
     app.controller("shortenController", ["$scope", "$http", shortenController]);
